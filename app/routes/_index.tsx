752290de71d2cdcb7c4a57c56a6381e29c62f2e3
tingzhao.ytz <tingzhao.ytz@antgroup.com>
2024-01-22 19:31:01 +0800

    feat: use @ant-design/web3

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,43 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Button } from "antd";
+import { Address } from "@ant-design/web3";
+import { WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
+import { createConfig, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+const config = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const App: React.FC = () => {
+  return (
+    <WagmiWeb3ConfigProvider config={config}>
+      <Address address="0x888" />
+    </WagmiWeb3ConfigProvider>
+  );
 };
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-        <Button>test antd</Button>
-      </ul>
-    </div>
-  );
-}
+export default App;
